d17e0a1752b1a3c0804e63cc2f9de738a189c04c
Kyle Machulis <kyle@nonpolynomial.com>
2012-12-11 23:31:40 -0800

    Added more tests

---
+++ fuckeverything/bigtest.py
@@ -0,0 +1,66 @@
+import sys
+import random
+import gevent
+import gevent.socket
+from messages import Message, MessageGenerator
+
+def testClient(num):
+    s = gevent.socket.create_connection(("localhost", 12345))
+    m = Message()
+    mg = MessageGenerator()
+    
+    # Request server info
+    m.msgtype = 0
+    s.send(m.rawData())
+    q = s.recv(100)
+    mg.addData(q)
+    g = mg.generate()
+    msg2 = g.next()
+    print msg2.msgtype
+    print msg2.value
+    
+    # get plugin list
+    m.msgtype = 1
+    s.send(m.rawData())
+    q = s.recv(100)
+    mg.addData(q)
+    msg2 = g.next()
+    print msg2.msgtype
+    print msg2.value
+    
+    # get device list
+    m.msgtype = 100
+    s.send(m.rawData())
+    q = s.recv(100)
+    mg.addData(q)
+    msg2 = g.next()
+    print msg2.msgtype
+    print msg2.value
+    
+    # claim device
+    m.msgtype = 1002
+    m.value = [0]
+    s.send(m.rawData())
+    
+    iterations = random.randrange(1000, 10000)
+    for i in range(0, iterations):
+        q = s.recv(100)
+        mg.addData(q)
+        msg2 = g.next()
+        if i % 100 == 0:
+            print "%d : %d" % (num, i)
+        # print msg2.msgtype
+        # print msg2.value
+        # m.msgtype = 1001
+        # s.send(m.rawData())
+        # time.sleep(1)
+    s.close()
+
+def main():
+    z = []
+    for i in range(0, 1000):
+        z.append(gevent.spawn(testClient,i))
+    gevent.joinall(z)
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ fuckeverything/rt-test.py
@@ -0,0 +1,50 @@
+from socket import socket
+from messages import Message, MessageGenerator
+import time
+
+s = socket()
+s.connect(("localhost", 12345))
+m = Message()
+mg = MessageGenerator()
+
+# Request server info
+m.msgtype = 0
+s.send(m.rawData())
+q = s.recv(100)
+mg.addData(q)
+g = mg.generate()
+msg2 = g.next()
+print msg2.msgtype
+print msg2.value
+
+# get plugin list
+m.msgtype = 1
+s.send(m.rawData())
+q = s.recv(100)
+mg.addData(q)
+msg2 = g.next()
+print msg2.msgtype
+print msg2.value
+
+# get device list
+m.msgtype = 100
+s.send(m.rawData())
+q = s.recv(100)
+mg.addData(q)
+msg2 = g.next()
+print msg2.msgtype
+print msg2.value
+
+# claim device
+m.msgtype = 1002
+m.value = [0]
+s.send(m.rawData())
+
+#m.msgtype = "RealTouchCDKString"
+m.msgtype = 9999
+m.value = ["V 255 U OUT 1000"]
+s.send(m.rawData())
+
+time.sleep(.1)
+
+s.close()
