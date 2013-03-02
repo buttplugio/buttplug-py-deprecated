6e02e9095fe996fa39039b98afddc1f00690aa9d
Kyle Machulis <kyle@nonpolynomial.com>
2013-03-01 23:56:53 -0800

    Added command line argument parsing in config and first tests

--- fuckeverything/config.py
+++ fuckeverything/config.py
@@ -1,20 +1,32 @@
 import os
 import sys
 import argparse
+from os.path import expanduser
 
-CONF_DIR = os.path.join("/home/qdot/.fuckeverything")
+CONF_DIR = os.path.join(expanduser("~"), ".fuckeverything")
 PLUGIN_DIR = os.path.join(CONF_DIR, "plugins")
 # SERVER_ADDRESS = "ipc://fetest"
 SERVER_ADDRESS = "tcp://127.0.0.1:9389"
 PING_RATE = 1
 PING_MAX = 3
 
-def init_arguments():
-    
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="FuckEverything")
+    parser.add_argument('--server_address', metavar='p', type=str,
+                        help='Address to listen on', default="tcp://127.0.0.1:9389")
+    args = parser.parse_args()
+
+    if args.server_address:
+        server_port = args.server_address
+    return True
+
 
 def init_config():
     """Initialize configuration values for server"""
+    parse_args()
     if not os.path.exists(CONF_DIR):
         raise RuntimeError("Configuration directory does not exist!")
     if not os.path.exists(PLUGIN_DIR):
         raise RuntimeError("Plugin directory does not exist!")
+
--- fuckeverything/test.py
+++ fuckeverything/test.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import mock
 sys.path.append("/home/qdot/code/git-projects/fuckeverything")
 from fuckeverything import config
 # from fuckeverything import plugin
@@ -7,12 +8,26 @@ from fuckeverything import config
 # from fuckeverything import system
 # from fuckeverything import heartbeat
 
+# argparse sys.argv testing using mock
+# via http://www.reddit.com/r/Python/comments/r9o2i/using_nose_and_argparse/
+# import mock
+# with mock.patch('sys.argv', ['whatever', 'I', 'wanna', 'test']):
+#         pass   # do whatever you want
+
 
 class ConfigTests(unittest.TestCase):
 
     def setUp(self):
         reload(config)
 
-    def testServer(self):
-        config.SERVER_ADDRESS = 'a'
-        self.failIf(config.SERVER_ADDRESS != 'a')
+    def testHelp(self):
+        with mock.patch('sys.argv', ['-h']):
+            config.init_config()
+
+    def testDirectoryCreation(self):
+        pass
+
+    def testInvalidConfiguration(self):
+        pass
+
+
