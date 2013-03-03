6ddf6f2f612c8be35d3eeeb07d6f5237bc3d1231
Kyle Machulis <kyle@nonpolynomial.com>
2013-03-02 17:27:17 -0800

    Code and tests for configuration file system

--- fuckeverything/config.py
+++ fuckeverything/config.py
@@ -1,32 +1,79 @@
 import os
-import sys
+import json
 import argparse
+import logging
 from os.path import expanduser
 
-CONF_DIR = os.path.join(expanduser("~"), ".fuckeverything")
-PLUGIN_DIR = os.path.join(CONF_DIR, "plugins")
-# SERVER_ADDRESS = "ipc://fetest"
-SERVER_ADDRESS = "tcp://127.0.0.1:9389"
-PING_RATE = 1
-PING_MAX = 3
+_cdirs = {"config": os.path.join(expanduser("~"), ".fuckeverything"),
+          "plugin": os.path.join(expanduser("~"), ".fuckeverything", "plugins")}
+
+_default_server_addr = "tcp://127.0.0.1:9389"
+
+_config = {"server_address": _default_server_addr,
+           "ping_rate": 1,
+           "ping_max": 3}
+
+def load_config():
+    logging.debug("Loading JSON Config from %s", os.path.join(_cdirs["config"], "config.json"))
+    with open(os.path.join(_cdirs["config"], "config.json"), "r+") as f:
+        j = json.load(f)
+        for (k, v) in j.items():
+            if k in _config:
+                _config[k] = v
+            else:
+                raise KeyError("Unexpected key %s found in configuration file!" % k)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="FuckEverything")
-    parser.add_argument('--server_address', metavar='p', type=str,
-                        help='Address to listen on', default="tcp://127.0.0.1:9389")
-    args = parser.parse_args()
-
-    if args.server_address:
-        server_port = args.server_address
-    return True
+def save_config():
+    logging.debug("Saving JSON Config to %s", os.path.join(_cdirs["config"], "config.json"))
+    with open(os.path.join(_cdirs["config"], "config.json"), "w") as f:
+        json.dump(_config, f)
 
 
 def init_config():
     """Initialize configuration values for server"""
-    parse_args()
-    if not os.path.exists(CONF_DIR):
-        raise RuntimeError("Configuration directory does not exist!")
-    if not os.path.exists(PLUGIN_DIR):
-        raise RuntimeError("Plugin directory does not exist!")
+    parser = argparse.ArgumentParser(description="FuckEverything")
+    parser.add_argument('--server_address', metavar='addr', type=str,
+                        help='Address to listen on', default=_config["server_address"])
+    parser.add_argument('--config_dir', metavar='path', type=str,
+                        help='configuration directory', default=_cdirs["config"])
+    parser.add_argument('--config_no_create_dir', action="store_true",
+                        help='configuration directory')
+    args = parser.parse_args()
 
+    if args.config_dir:
+        _cdirs["config"] = os.path.abspath(args.config_dir)
+        _cdirs["plugin"] = os.path.join(_cdirs["config"], "plugins")
+    if not os.path.exists(_cdirs["config"]):
+        if args.config_no_create_dir:
+            raise RuntimeError("Configuration directory does not exist!")
+        logging.debug("Creating directory %s", _cdirs["config_dir"])
+        os.makedirs(_cdirs["config"])
+    if not os.path.exists(os.path.join(_cdirs["config"], "config.json")):
+        save_config()
+    else:
+        load_config()
+    if not os.path.exists(_cdirs["plugin"]):
+        if args.config_no_create_dir:
+            raise RuntimeError("Plugin directory does not exist!")
+        logging.debug("Creating directory %s", _cdirs["plugin"])
+        os.makedirs(_cdirs["plugin"])
+
+    # only reset the server address if it's not the default
+    if args.server_address != _default_server_addr:
+        set_config_value("server_address", args.server_address)
+
+
+def get_config_value(key):
+    if key not in _config.keys():
+        raise KeyError("Config value %s does not exist!" % key)
+    logging.debug("Config value %s is %s", key, _config[key])
+    return _config[key]
+
+
+def set_config_value(key, value):
+    if key not in _config.keys():
+        raise KeyError("Config value %s does not exist!" % key)
+    _config[key] = value
+    logging.debug("Setting config value %s to %s", key, _config[key])
+    save_config()
--- fuckeverything/test.py
+++ fuckeverything/test.py
@@ -1,5 +1,7 @@
 import sys
+import os
 import unittest
+import shutil
 import mock
 sys.path.append("/home/qdot/code/git-projects/fuckeverything")
 from fuckeverything import config
@@ -21,13 +23,81 @@ class ConfigTests(unittest.TestCase):
         reload(config)
 
     def testHelp(self):
-        with mock.patch('sys.argv', ['-h']):
-            config.init_config()
+        """Help test. Should just bail out."""
+        with mock.patch('sys.argv', ['fuckeverything', '-h']):
+            try:
+                config.init_config()
+                self.fail("Not throwing exception to quit program after help list!")
+            except SystemExit:
+                pass
 
     def testDirectoryCreation(self):
-        pass
+        """Create a new config directory and populate it"""
+        import tempfile
+        d = tempfile.mkdtemp()
+        with mock.patch('sys.argv', ['fuckeverything', '--config_dir', d]):
+            config.init_config()
+            self.failIf(not os.path.exists(d))
+            self.failIf(not os.path.exists(config._cdirs["config"]))
+            self.failIf(not os.path.exists(config._cdirs["plugin"]))
+        shutil.rmtree(d)
 
-    def testInvalidConfiguration(self):
-        pass
+    def testDirectoryNoCreation(self):
+        """Do not create new directory if it doesn't exist, fail out instead"""
+        import tempfile
+        d = tempfile.mkdtemp()
+        with mock.patch('sys.argv', ['fuckeverything', '--config_dir', d, "--config_no_create_dir"]):
+            try:
+                config.init_config()
+                self.fail("Not throwing exception on missing configuration directories!")
+            except RuntimeError:
+                pass
+        shutil.rmtree(d)
 
+    def testInvalidConfigValue(self):
+        """Test that we throw when we get a bad config value"""
+        import tempfile
+        d = tempfile.mkdtemp()
+        with mock.patch('sys.argv', ['fuckeverything', '--config_dir', d]):
+            config.init_config()
+            try:
+                config.get_config_value("testing")
+                self.fail("Not throwing expection on missing configuration option!")
+            except KeyError:
+                pass
+        shutil.rmtree(d)
+
+    def testValidConfigValue(self):
+        """Test that we get a matching config value"""
+        import tempfile
+        d = tempfile.mkdtemp()
+        with mock.patch('sys.argv', ['fuckeverything', '--config_dir', d]):
+            config.init_config()
+            v = config.get_config_value("server_address")
+            self.failIf(v != config._config["server_address"])
+        shutil.rmtree(d)
+
+    def testConfigFileCreation(self):
+        """Test that we create config files correctly"""
+        import tempfile
+        d = tempfile.mkdtemp()
+        with mock.patch('sys.argv', ['fuckeverything', '--config_dir', d]):
+            config.init_config()
+            self.failIf(not os.path.exists(os.path.join(config._cdirs["config"], "config.json")))
+        shutil.rmtree(d)
+
+    def testConfigFileLoad(self):
+        """Test that we load config files correctly"""
+        import tempfile
+        d = tempfile.mkdtemp()
+        with mock.patch('sys.argv', ['fuckeverything', '--config_dir', d]):
+            config.init_config()
+            config.set_config_value("server_address", "ipc://wat")
+            reload(config)
+            config.init_config()
+            self.failIf(config.get_config_value("server_address") != "ipc://wat")
+        shutil.rmtree(d)
+
+    def testInvalidConfigurationFileLoad(self):
+        pass
 
