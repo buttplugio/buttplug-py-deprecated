f931bf24af7dbef745c06e5913e8894f2218d3f7
Kyle Machulis <kyle@nonpolynomial.com>
2013-03-07 23:24:25 -0800

    Changed server to have seperate init and msg_loop functions for easier systems testing

--- fuckeverything/server.py
+++ fuckeverything/server.py
@@ -10,52 +10,59 @@ from gevent_zeromq import zmq
 import msgpack
 import logging
 
+# Name a global one underscore off from a module? Why not.
+_zmq = {}
+
+
+def init():
+    _zmq["context"] = zmq.Context()
+    queue.init(_zmq["context"])
+    _zmq["router"] = _zmq["context"].socket(zmq.ROUTER)
+    _zmq["router"].bind(config.get_value("server_address"))
+    _zmq["queue"] = _zmq["context"].socket(zmq.PULL)
+    _zmq["queue"].connect(queue.QUEUE_ADDRESS)
+    _zmq["poller"] = zmq.Poller()
+    _zmq["poller"].register(_zmq["router"], zmq.POLLIN)
+    _zmq["poller"].register(_zmq["queue"], zmq.POLLIN)
+
+
+def msg_loop():
+    socks = dict(_zmq["poller"].poll())
+
+    if _zmq["router"] in socks and socks[_zmq["router"]] == zmq.POLLIN:
+        identity = _zmq["router"].recv()
+        msg = _zmq["router"].recv()
+        system.parse_message(identity, msgpack.unpackb(msg))
+
+    if _zmq["queue"] in socks and socks[_zmq["queue"]] == zmq.POLLIN:
+        identity = _zmq["queue"].recv()
+        msg = _zmq["queue"].recv()
+        _zmq["router"].send(identity, zmq.SNDMORE)
+        _zmq["router"].send(msg)
+
 
 def start():
     """Start server loop"""
     # Bring up logging, fill out configuration values
     logging.basicConfig(level=logging.DEBUG)
     config.init()
-
-    # Start zmq server
-    context = zmq.Context()
-    queue.init(context)
-    socket_router = context.socket(zmq.ROUTER)
-    socket_router.bind(config.get_value("server_address"))
-    socket_queue = context.socket(zmq.PULL)
-    socket_queue.connect(queue.QUEUE_ADDRESS)
-    poller = zmq.Poller()
-    poller.register(socket_router, zmq.POLLIN)
-    poller.register(socket_queue, zmq.POLLIN)
+    init()
 
     # Start plugins
     plugin.get_device_list()
     plugin.scan_for_plugins()
     plugin.start_plugin_counts()
-    if(logging.getLogger().getEffectiveLevel() == logging.DEBUG):
-        logging.debug("Plugins found:")
-        logging.debug(plugin.plugins_available())
-        for plin in plugin.plugins_available():
-            logging.debug(plin)
-    #plugin.scan_for_devices(True)
+    logging.debug("Plugins found:")
+    logging.debug(plugin.plugins_available())
+    for plin in plugin.plugins_available():
+        logging.debug(plin)
 
     # Run Loop
     try:
         while True:
-            socks = dict(poller.poll())
-
-            if socket_router in socks and socks[socket_router] == zmq.POLLIN:
-                identity = socket_router.recv()
-                msg = socket_router.recv()
-                system.parse_message(identity, msgpack.unpackb(msg))
-
-            if socket_queue in socks and socks[socket_queue] == zmq.POLLIN:
-                identity = socket_queue.recv()
-                msg = socket_queue.recv()
-                socket_router.send(identity, zmq.SNDMORE)
-                socket_router.send(msg)
+            msg_loop()
     except KeyboardInterrupt:
-        socket_router.close()
+        _zmq["router"].close()
     process.kill_all()
     event.kill_all()
     utils.gevent_join()
