47ca6c8309411d6cb28a8efcfc92177de1be8231
Kyle Machulis <kyle@nonpolynomial.com>
2013-03-09 00:13:18 -0800

    Added removal list to heartbeat

--- fuckeverything/heartbeat.py
+++ fuckeverything/heartbeat.py
@@ -5,19 +5,33 @@ from fuckeverything.utils import gevent_func, FEShutdownException
 import gevent
 import logging
 
+_removal = []
+
+def _check_removal(identity):
+    if identity in _removal:
+        _removal.remove(identity)
+        return True
+    return False
+
 
 @gevent_func
 def start(identity):
     while True:
+        if _check_removal(identity):
+            return False
         e = event.add(identity, "FEPing")
         queue.add(identity, ["s", "FEPing"])
         try:
             e.get(block=True, timeout=config.get_value("ping_max"))
         except gevent.Timeout:
-            logging.debug("identity %s died", identity)
+            if not _check_removal(identity):
+                logging.debug("identity %s died", identity)
             return False
         except FEShutdownException:
             logging.debug("FE Closing, shutting down")
+            return False
+
+        if _check_removal(identity):
             return False
 
         # Add a bogus messages called PingWait. We should never get a reply to
@@ -33,3 +47,7 @@ def start(identity):
             logging.debug("FE Closing, shutting down")
             break
         event.remove(identity, "FEPingWait")
+
+
+def remove(identity):
+    _removal.append(identity)
