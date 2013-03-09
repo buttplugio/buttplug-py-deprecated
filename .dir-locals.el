4b80869c13860aa96b026d6951ea1855f51c1fb5
Kyle Machulis <kyle@nonpolynomial.com>
2013-03-08 22:09:04 -0800

    Added gross dir-locals to fix search directories for fe

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;; http://www.emacswiki.org/emacs/DirectoryVariables
+
+((nil .
+			((eval . (setq qdot/project-directory (locate-dominating-file buffer-file-name ".dir-locals.el")))))
+ (python-mode .
+							((eval .
+										 (setq proj-args (list "--sys-path" (expand-file-name "fuckeverything" qdot/project-directory))))
+							 (eval .
+										 (set (make-local-variable 'jedi:server-args) proj-args)))))
