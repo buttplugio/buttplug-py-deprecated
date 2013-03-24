9e191538929d3ecfc200d66c7b43c3f64658d13f
Kyle Machulis <kyle@nonpolynomial.com>
2013-03-23 17:11:24 -0700

    Update setup file to work with cxfreeze, add building for plugin and client

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import sys
+
+from cx_Freeze import setup, Executable
+includes = ["zmq", "gevent", "greenlet", "msgpack"]
+
+base = None
+include_files = []
+if sys.platform == "win32":
+    # For the moment, just make this run as a console application on windows
+    # base = "Win32GUI"
+
+    # Hardcode the libzmq path because fuck it. Just make sure it gets copied on
+    # windows.
+    include_files.append(("c:/Python27/Lib/site-packages/pyzmq-13.0.0-py2.7-win32.egg/zmq/libzmq.pyd", "libzmq.pyd"))
 
 setup(name='FuckEverything',
       version='0.01',
       description='FuckEverything Sex Toy Control Server',
       author='Kyle Machulis',
       author_email='kyle@nonpolynomial.com',
-      url='http://www.slashdong.org/',
+      url='http://www.feverything.com/',
       packages=['fuckeverything'],
-      scripts=['scripts/fuckeverything']
-     )
+      scripts=['scripts/fuckeverything', 'scripts/test-plugin', 'scripts/test-client'],
+      options={"build_exe": {"includes": includes, "include_files": include_files}},
+      executables=[Executable("scripts/fuckeverything", base=base), Executable("scripts/test-client", base=base), Executable("scripts/test-plugin", base=base)]
+)
