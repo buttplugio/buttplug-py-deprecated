fc339d1c5e3cc2c50884c22f9ae401a11c87e51a
Kyle Machulis <kyle@nonpolynomial.com>
2013-03-31 00:24:17 -0700

    Indentation fix

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
 ((nil .
       ((eval . (setq qdot/project-directory (locate-dominating-file buffer-file-name ".dir-locals.el")))))
  (python-mode .
-	      ((eval .
-		     (setq proj-args (list "--sys-path" (expand-file-name "fuckeverything" qdot/project-directory) "--sys-path" qdot/project-directory)))
-	       (eval .
-		     (set (make-local-variable 'jedi:server-args) proj-args)))))
+              ((eval .
+                     (setq proj-args (list "--sys-path" (expand-file-name "fuckeverything" qdot/project-directory) "--sys-path" qdot/project-directory)))
+               (eval .
+                     (set (make-local-variable 'jedi:server-args) proj-args)))))
